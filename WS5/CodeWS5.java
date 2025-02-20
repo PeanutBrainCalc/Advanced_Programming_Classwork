412c14b7e23c0937125061a5ecc774ed84882548
PeanutBrainCalc <rekharani23969@gmail.com>
2025-02-21 04:17:38 +0530

    Create CodeWS5.java
    Added code for Worksheet 5

---
+++ WS5/CodeWS5.java
@@ -0,0 +1,61 @@
+//Same tree  https://leetcode.com/problems/same-tree/submissions/1542504413
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == q) return true;
+        if (p == null || q == null || p.val != q.val) return false;
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
+}
+
+
+
+
+//Path Sum https://leetcode.com/problems/path-sum/submissions/1542504928
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean hasPathSum(TreeNode root, int targetSum) {
+        return dfs(root, targetSum);
+    }
+
+    private boolean dfs(TreeNode root, int s) {
+        if (root == null) {
+            return false;
+        }
+        s -= root.val;
+        if (root.left == null && root.right == null && s == 0) {
+            return true;
+        }
+        return dfs(root.left, s) || dfs(root.right, s);
+    }
+}
